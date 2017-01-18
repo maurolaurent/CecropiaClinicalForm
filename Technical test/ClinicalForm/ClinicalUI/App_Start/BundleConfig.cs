d28eefb25d5e3d8bb7e76bf3bc1c2f7df6cebb7a
Mauro Laurent <maurolaurent@gmail.com>
2017-01-18 09:41:36 -0600

    Addind app.Start

---
+++ Technical test/ClinicalForm/ClinicalUI/App_Start/BundleConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace ClinicalUI
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkID=303951
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+                            "~/Scripts/WebForms/WebForms.js",
+                            "~/Scripts/WebForms/WebUIValidation.js",
+                            "~/Scripts/WebForms/MenuStandards.js",
+                            "~/Scripts/WebForms/Focus.js",
+                            "~/Scripts/WebForms/GridView.js",
+                            "~/Scripts/WebForms/DetailsView.js",
+                            "~/Scripts/WebForms/TreeView.js",
+                            "~/Scripts/WebForms/WebParts.js"));
+
+            // Order is very important for these files to work, they have explicit dependencies
+            bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/DevExpress").Include(
+                    "~/Scripts/Lib/js/jquery-2.2.3.min.js",
+                    "~/Scripts/Lib/js/dx.all.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Bootstrap").Include(
+                    "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ClinicalForm").Include(
+                    "~/Scripts/User/ajaxFuncs.js",
+                    "~/Scripts/User/initClinicalForms.js"
+                   ));
+
+            // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                            "~/Scripts/modernizr-*"));
+
+            ScriptManager.ScriptResourceMapping.AddDefinition(
+                "respond",
+                new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/respond.min.js",
+                    DebugPath = "~/Scripts/respond.js",
+                });
+        }
+    }
+}
---
+++ Technical test/ClinicalForm/ClinicalWebService/App_Start/WebApiConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+using ClinicalLib;
+    
+
+namespace ClinicalWebService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<BloodType>("BloodTypes");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            // Force to ignore Request Content Type Header and reply only JSON
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute("CustomApi", "api/{controller}/{action}/{id}", new { id = RouteParameter.Optional }
+         );
+        }
+    }
+}
